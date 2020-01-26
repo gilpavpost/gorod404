aea5c48fdcbf7c0598dc9a84a1fdde1b664eb26d
gilpavdev <gilpavpost@gmail.com>
2020-01-27 01:15:48 +0300

    first version

---
+++ app/js/timer.js
@@ -0,0 +1,21 @@
+
+var sec=00;
+// выставляем минуты
+var min=30;
+
+function refresh()
+{
+	sec--;
+	if(sec==-01){sec=59; min=min-1;}
+	else{min=min;}
+	if(sec<=9){sec="0" + sec;}
+	time=(min<=9 ? "0"+min : min) + ":" + sec;
+	if(document.getElementsByClassName){timer.innerHTML=time;}
+	inter=setTimeout("refresh()", 1000);
+	// action if time 00:00
+	if(min=='00' && sec=='00'){
+		sec="00";
+		clearInterval(inter);
+        location.reload(true);
+	}
+}
